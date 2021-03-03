03fc534e88405d84aadda2724b06f211a43b1dec
Aubrey Powell <herufeanor@gmail.com>
2021-03-03 11:47:45 -0800

    In the UIExample, have nextId instead of maxId
    The identifier maxId implies that it's the maximum ID that's been
    assigned, when it's actually not. It's the next ID to be assigned, which
    is one greater than the current maximum. So nextId is more accurate.

--- Examples/UI/State.cs
+++ Examples/UI/State.cs
@@ -4,15 +4,15 @@ using System.Collections.Generic;
 namespace UIExample {
 	public class State {
 		public Dictionary<int, List<string>> lists = new Dictionary<int, List<string>>();
-		public int maxId = 0;
+		public int nextId = 0;
 
 		public void Apply(Action<State> action) {
 			action(this);
 		}
 
 		public static Action<State> AddList() => state => {
-			state.lists[state.maxId] = new List<string>();
-			state.maxId++;
+			state.lists[state.nextId] = new List<string>();
+			state.nextId++;
 		};
 
 		public static Action<State> RemoveList(int listId) => state => {
