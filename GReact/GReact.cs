e48ef24ae4cd4a0b24cc00b844447936ff33ec74
Aubrey Powell <herufeanor@gmail.com>
2021-03-22 18:41:00 -0700

    Add Renderer.Cleanup()
    If you ever need to get rid of a renderer before the program closes,
    it's important to call Cleanup() to remove the portion of the scene
    graph it managed.

--- GReact/GReact.cs
+++ GReact/GReact.cs
@@ -62,6 +62,10 @@ namespace GReact {
 			oldRootElem = popElem;
 		}
 
+		public void Cleanup() {
+			oldRootElem?.node.QueueFree();
+		}
+
 		private int GetNodeHierarchySize(Godot.Node node) {
 			var size = 1;
 			for (int i = 0; i < node.GetChildCount(); i++) {
